9752fe2579d03d40371b9e566b851779296402d0
razakhadim <53213155+razakhadim@users.noreply.github.com>
2020-11-05 21:55:27 +1300

    Update user.class.php

--- classes/user.class.php
+++ classes/user.class.php
@@ -3,77 +3,95 @@
 class User extends Database
 {
 
-    private $fname;
-    private $lname;
-    private $email;
-    private $password;
-    private $role;
-    private $address;
-    private $phone;
-    private $profileImage;
+   private $fname;
+   private $lname;
+   private $email;
+   private $password;
+   private $role;
+   private $address;
+   private $phone;
+   private $profileImage;
 
-    //getters & setters
+   //getters & setters
 
-    public function getFirstName(){
-        return $this->fname;
-    }
+   public function getFirstName()
+   {
+      return $this->fname;
+   }
 
-    public function setFirstName($fname){
-       return $this->fname = $fname;
-    }
-    public function setLastName($lname){
-        return $this->lname = $lname;
-     }
-     public function setEmail($email){
-        return $this->email = $email;
-     }
-     public function setPassword($password){
-        return $this->password = $password;
-     }
-     public function setRole($role){
-        return $this->role = $role;
-     }
-     public function setAddress($address){
-        return $this->address = $address;
-     }
-     public function setPhone($phone){
-        return $this->phone = $phone;
-     }
-     public function setProfileImage($profileImage){
-        return $this->profileImage = $profileImage;
-     }
+   public function setFirstName($fname)
+   {
+      return $this->fname = $fname;
+   }
+   public function setLastName($lname)
+   {
+      return $this->lname = $lname;
+   }
+   public function setEmail($email)
+   {
+      return $this->email = $email;
+   }
+   public function setPassword($password)
+   {
+      return $this->password = $password;
+   }
+   public function setRole($role)
+   {
+      return $this->role = $role;
+   }
+   public function setAddress($address)
+   {
+      return $this->address = $address;
+   }
+   public function setPhone($phone)
+   {
+      return $this->phone = $phone;
+   }
+   public function setProfileImage($profileImage)
+   {
+      return $this->profileImage = $profileImage;
+   }
 
 
-    public function add()
-    {
+   public function add()
+   {
 
-        $sql = "INSERT INTO users (user_fname, user_lname, user_password, user_email, user_address, user_contact_number) VALUES ('$this->fname', '$this->lname', '$this->password', '$this->email', '$this->address', '$this->phone')";
-        $result = $this->connect()->query($sql);
+      $sql = "INSERT INTO users (user_fname, user_lname, user_password, user_email, user_address, user_contact_number) VALUES ('$this->fname', '$this->lname', '$this->password', '$this->email', '$this->address', '$this->phone')";
+      $result = $this->connect()->query($sql);
 
-        if (!$result) {
-            echo "database Something went wrong";
-        }
-    }
+      if (!$result) {
+         echo "database Something went wrong";
+      }
+   }
 
-    public function getAllUsers()
-    {
+   public function getAllUsers()
+   {
       return $this->allRecords("SELECT * FROM users");
-    }
+   }
 
-    public function getUserById($id)
-    {
+   public function getUserById($id)
+   {
+     $result = $this->single("SELECT * FROM users WHERE user_id ={$id}");
 
-        $this->single("SELECT FROM users WHERE user_id ={$id}");
+   //   if(is_array($result)) {
+   //   foreach ($result as $data) {
+       
+   //    $this->setFirstName($data['user_fname']);
+   //    $userFname = $data['user_fname'];
+   //    $userLname = $data['user_lname'];
+   //    $userEmailId = $data['user_email'];
+   //    $userRole = $data['user_role'];
+   //   }
+   // }
+   }
 
-    }
+   public function updateUser($id)
+   {
+      $this->update("UPDATE users SET user_fname = '$this->fname', user_lname = '$this->lname', user_address ='$this->address', user_contact_number = '$this->phone' WHERE user_id = {$id}");
+   }
 
-    public function updateUser($id)
-    {
-    $this->update("UPDATE users SET user_fname = '$this->fname', user_lname = '$this->lname', user_address ='$this->address', user_contact_number = '$this->phone' WHERE user_id = {$id}");
-    }
-
-    public function deleteUser($id)
-    {
-        $this->delete("DELETE FROM users where user_id =" . $id);
-    }
+   public function deleteUser($id)
+   {
+      $this->delete("DELETE FROM users where user_id =" . $id);
+   }
 }
