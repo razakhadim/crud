2f83bba9bb6679b530ecf6dcbd1195cdda44f831
razakhadim <53213155+razakhadim@users.noreply.github.com>
2020-11-05 21:54:26 +1300

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,16 +6,14 @@ include ('classes/product.class.php');
 include ('classes/user.class.php');
 
 $user = new User();
+$product = new Product();
 
 if (isset($_GET['uid'])) {
     $id = $_GET['uid'];
     $user->deleteUser($id);
 
+} else if (isset($_GET['pid'])) {
+     $id = $_GET['pid'];
+     $product->delete($id);
 }
 
-$product = new Product();
-
-if (isset($_GET['pid'])) {
-    $id = $_GET['pid'];
-    $product->delete($id);
-}
